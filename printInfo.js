3bdda37234736443fd611baddce249d0d59bfe27
Ivo Krastev <52259854+BrascoBG@users.noreply.github.com>
2020-01-09 16:27:09 +0200

    Create printInfo.js

---
+++ printInfo.js
@@ -0,0 +1,4 @@
+function printInfo([firstName, lastName, age, town]){
+  console.log(`You are ${firstName} ${lastName}, a ${age} years old person, from ${town}. Aren't you?`);
+}
+printInfo(['Ivo', 'Krastev', 35, 'Plovdiv']);
